ab97ff8a7092cf3bc5f60a4cf0a1fe48f12fd777
tincho020602 <ezequiel.ibarra020602@gmail.com>
2024-03-26 12:13:48 -0300

    subir archivos

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <stdio.h>
 
 int main(){
     std::cout << "hola mundo" <<std::endl;
+    int variable=5;
+    int *puntero=&variable;
+    printf("\nEl contenido del puntero es: %d",puntero);
+    printf("\nLa direccion de memoria almacenada por: %d",puntero);
+
 }
