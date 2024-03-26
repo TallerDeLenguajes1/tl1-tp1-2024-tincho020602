4f97e36f47a7a46ce554df2e08914d418f5fd346
tincho020602 <ezequiel.ibarra020602@gmail.com>
2024-03-26 12:27:19 -0300

    punto 3 e)

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -5,7 +5,9 @@ int main(){
     std::cout << "hola mundo" <<std::endl;
     int variable=5;
     int *puntero=&variable;
-    printf("\nEl contenido del puntero es: %d",puntero);
-    printf("\nLa direccion de memoria almacenada por: %d",puntero);
-
+    printf("\nEl contenido del puntero es: %d",*puntero);
+    printf("\nLa direccion de memoria almacenada por el puntero es: %p",*puntero);
+    printf("\nLa direccion de memoria almacenada por la variable es: %p",variable);
+    printf("\nLa direccion del puntero es: %p",puntero);
+    printf("\nTamaño de memoria utilizado por la variable: %zu bytes\n", sizeof(variable));
 }
